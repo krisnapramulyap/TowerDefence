4e8ab256d7e202acf562a642e9314c414ca6c6c0
Krisna Pramulya <19560029@stmik.jayakarta.ac.id>
2021-09-19 23:15:58 +0700

    Tempat penyimpanan file Tower Defence

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour
+{
+    public void gantiscene(string scene){
+        SceneManager.LoadScene(scene);
+    }
+
+    public void exitgame(){
+        Application.Quit();
+
+    }
+    
+}
